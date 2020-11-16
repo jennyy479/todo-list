9f19f84038031d9d085be6b34ef28cf58c067853
Yen <z83825@yahoo.com.tw>
2020-11-17 00:40:01 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const Todo = require('./models/todo')
 
@@ -19,6 +20,8 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   //拿到全部todo
   Todo.find()
@@ -26,6 +29,19 @@ app.get('/', (req, res) => {
     .then(todos => res.render('index', { todos }))
     .catch(error => console.log(error))
 })
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  const todo = new Todo({ name })
+  return todo.save()
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000/')
