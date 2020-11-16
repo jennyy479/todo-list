a1c618d57cc67be458c7ef69eeab825cf3d90fe7
Yen <z83825@yahoo.com.tw>
2020-11-16 22:56:57 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-
+const exphbs = require('express-handlebars')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -13,8 +13,11 @@ db.once('open', () => {
   console.log('mongodb connected')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
 app.listen(3000, () => {
