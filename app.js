45315bc42d0102fb16e7f77182f7c34a47f5ac19
Yen <z83825@yahoo.com.tw>
2020-11-16 23:59:58 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars')
+
+const Todo = require('./models/todo')
+
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
@@ -17,7 +20,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  //拿到全部todo
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
