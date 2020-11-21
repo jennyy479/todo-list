1d3f868d8f59d9069942dd72b243f64f05dddeac
Yen <z83825@yahoo.com.tw>
2020-11-21 13:32:13 +0800

    feat: add bootstrap & modify main.hbs

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,10 +1,11 @@
 const Todo = require('../todo')
 
+
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
   for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` })
+    Todo.create({ name: 'name-' + i })
   }
   console.log('done')
 })
